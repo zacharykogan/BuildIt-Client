46fc2ade1e461d42380a33d579dc0dfcced30ca9
zacharykogan <86493264+zacharykogan@users.noreply.github.com>
2021-09-29 21:54:15 -0400

    warning-validatedomnesting-a-cannot-appear-as-a-descendant-of-a

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,75 +1,83 @@
 import React, { Fragment } from 'react'
 import { Nav, NavDropdown } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const myAccount = {
   align: 'right',
   color: 'white'
 }
 const authenticatedOptions = (
   <Fragment>
-    <NavLink to='/my-projects' className='nav-link'>
+    <Nav.Link as={Link} to='/my-projects' className='nav-link'>
       My Projects
-    </NavLink>
-    <NavLink to='/create-project' className='nav-link'>
+    </Nav.Link>
+    <Nav.Link as={Link} to='/create-project' className='nav-link'>
       Create a Project
-    </NavLink>
-    <NavLink to='/change-password' className='nav-link'>
+    </Nav.Link>
+    <Nav.Link as={Link} to='/change-password' className='nav-link'>
       Change Password
-    </NavLink>
-    <NavLink to='/sign-out' className='nav-link'>
+    </Nav.Link>
+    <Nav.Link as={Link} to='/sign-out' className='nav-link'>
       Sign Out
-    </NavLink>
+    </Nav.Link>
   </Fragment>
 )
 const unauthenticatedOptions = (
   <Fragment>
-    <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
+    <Nav.Link as={Link} to='/sign-in' className='nav-link'>
+      Sign In
+    </Nav.Link>
   </Fragment>
 )
 
 const alwaysOptions = (
   <Fragment>
-    <NavLink to='/' className='nav-link'>
+    <Nav.Link as={Link} to='/' className='nav-link'>
       Home
-    </NavLink>
-    <NavLink to='/projects' className='nav-link'>
+    </Nav.Link>
+    <Nav.Link as={Link} to='/projects' className='nav-link'>
       Projects
-    </NavLink>
+    </Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
   <Navbar sticky='top' bg='dark' variant='dark' expand='md'>
     <Navbar.Brand>
-      <NavLink to='/' style={{ color: '#FFF', textDecoration: 'none' }}>
+      <Nav.Link
+        as={Link}
+        to='/'
+        style={{ color: '#FFF', textDecoration: 'none' }}>
         🪜 BuildIT
-      </NavLink>
+      </Nav.Link>
     </Navbar.Brand>
     <Navbar.Toggle variant='dark' aria-controls='basic-navbar-nav' />
     <Navbar.Collapse variant='dark' id='basic-navbar-nav'>
-      <Nav className='ms-auto'>
-        {' '}
-        {alwaysOptions}
-      </Nav>
-      <NavDropdown
-        style={myAccount}
-        className='ml-auto'
-        variant='dark'
-        title='My Account '
-        id='nav-dropdown'>
-        🛠️
-        <NavDropdown.Item variant='dark' className='ml-auto'>
-          {user && (
-            <span className='navbar-text mr-2'>Welcome, {user.email.name}</span>
-          )}
-        </NavDropdown.Item>
-        <NavDropdown.Item variant='dark' href='#action/3.2'>
-          {user ? authenticatedOptions : unauthenticatedOptions}
-        </NavDropdown.Item>
-      </NavDropdown>
+      <Nav className='ms-auto'> {alwaysOptions}</Nav>
+      <>
+        <NavDropdown
+          style={myAccount}
+          variant='dark'
+          title='My Account '
+          id='nav-dropdown'>
+          <NavDropdown variant='dark'>
+            {user && (
+              <span className='navbar-text mr-2'>
+                Welcome, {user.email.name}
+              </span>
+            )}
+          </NavDropdown >
+          <NavDropdown.Item variant='dark'>
+            {user ? authenticatedOptions : unauthenticatedOptions}
+          </NavDropdown.Item>
+        </NavDropdown>
+      </>
     </Navbar.Collapse>
   </Navbar>
 )
 
 export default Header
+
+// <NavLink href="#x"><Link id="RouterNavLink" style={None} to="/contact">anywords</Link></NavLink>
+
+// <NavLink id="RouterNavLink" style={None} to="/contact">anywords</NavLink>
